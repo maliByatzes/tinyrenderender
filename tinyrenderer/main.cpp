46071111c82fbb4ec0d2dc940cf56a6f77b2005f
maliByatzes <malib2027@gmail.com>
2024-07-31 17:43:33 +0200

    add tga image library

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include "tgaimage.h"
 
-int main (int argc, char* argv[])
-{
-  using namespace std;
+const TGAColor white{255, 255, 255, 255};
+const TGAColor red{255, 0, 0, 255};
 
-  if (argc < 2)
-  {
-    cerr << "error: missing name" << endl;
-    return 1;
-  }
-
-  cout << "Hello, " << argv[1] << '!' << endl;
+int main(int argc, char **argv) {
+  TGAImage image(100, 100, TGAImage::RGB);
+  image.set(52, 41, red);
+  image.flip_vertically(); // i want to have the origin at the left bottom
+                           // corner of the image
+  image.write_tga_file("output.tga");
+  return 0;
 }
+
